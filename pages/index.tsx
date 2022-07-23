0d5e3a831eba125d828925062fdcc29587bdb75b
Pier Daniele Cretara <pierdanielecretara@lviiier.com>
2022-07-23 14:50:07 +0200

    Add Google Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,34 @@
-import type { NextPage } from "next";
+import type {NextPage} from "next";
 import Head from "next/head";
+import Script from "next/script";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-blue-600 flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Cretara.dev site</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="bg-blue-600 flex min-h-screen flex-col items-center justify-center py-2">
+            <Head>
+                <title>Cretara.dev site</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center text-white">
-        <h1 className="text-6xl font-bold">Welcome to Cretara.dev</h1>
-      </main>
-    </div>
-  );
+            <Script strategy="lazyOnload"
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+
+            <Script strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center text-white">
+                <h1 className="text-6xl font-bold">Welcome to Cretara.dev</h1>
+            </main>
+        </div>
+    );
 };
 
 export default Home;
